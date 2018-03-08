a38b5fa611a49d36be71e072daad61033f262a74
ProgrammingIncluded <chencharlesh@gmail.com>
2018-03-07 16:51:00 -0800

    Updated program to python 3.6

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from TFE import *
 import time
 import random as rnd
 import sys
+import random
 
 
 def main():
@@ -16,26 +17,26 @@ def main():
     # generate a new
     tfe.putNew()
     tfe.putNew()
-    print "STARTING BOARD: "
+    print("STARTING BOARD: ")
     # tfe.grid = np.array([0, 0, 2, 16, 0 ,0 , 64, 4, 0, 8, 16, 256, 2,4, 2, 16]).reshape((4,4))
-    print tfe.grid
-    print ""
+    print(tfe.grid)
+    print("")
 
-    act = 'd'
     while (not tfe.isWin()) and (not tfe.isLose()):
+        act = MOV_OPT[random.randint(0, 3)]
 
         start = time.clock() 
 
-        print "*********************"
+        print("*********************")
 
-        print "AI SELECT ACTION: " + act
-        print "*********************"
-        print "BEFORE: "
-        print tfe.grid
-        print ""
+        print("AI SELECT ACTION: " + str(act))
+        print("*********************")
+        print("BEFORE: ")
+        print(tfe.grid)
+        print("")
 
-        print "*********************"
-        print "AFTER: "
+        print("*********************")
+        print("AFTER: ")
 
         # move grid
         tfe.moveGrid(act)
@@ -43,25 +44,25 @@ def main():
         # generate a new
         tfe.putNew()
 
-        print tfe.grid
-        print ""
+        print(tfe.grid)
+        print("")
 
-        print "TIME TAKEN FOR STEP: " + str(time.clock() - start)
-        print ""
+        print("TIME TAKEN FOR STEP: " + str(time.clock() - start))
+        print("")
         # Flush it
         sys.stdout.flush()
     
-    print "FINISHED: "
-    print tfe.grid
-    print ""
+    print("FINISHED: ")
+    print(tfe.grid)
+    print("")
 
-    print "IS WIN?: "
-    print tfe.isWin()
-    print ""
+    print("IS WIN?: ")
+    print(tfe.isWin())
+    print("")
     
-    print "IS LOSE?: "
-    print tfe.isLose()
-    print ""
+    print("IS LOSE?: ")
+    print(tfe.isLose())
+    print("")
 
 
 if __name__ == '__main__':
